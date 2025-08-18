4e2a1381cac8f0d5a1422fe00e9ef5e2252afdce
Eusebius Marcel <emarcel@paystone.com>
2025-08-18 08:09:58 +0700

    Migrate to Vite from CRA

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,11 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables");
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);

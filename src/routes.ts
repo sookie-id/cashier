ac06088633199ca342a1843da91dc2eed5cf1903
Eusebius Marcel <emarcel@paystone.com>
2025-08-22 20:12:58 +0700

    Create other pages

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  route("login", "./auth/presentation/Login.tsx"),
-  // * matches all URLs, the ? makes it optional so it will match / as well
-  route("*?", "./sales/presentation/CreateTransaction.tsx"),
+  route("/", "./sales/presentation/CreateTransaction.tsx"),
+  route("/login", "./auth/presentation/Login.tsx"),
+  route("/sales", "./sales/presentation/SalesList.tsx"),
+  route("/customers", "./customers/presentation/CustomerList.tsx"),
+  route("/menu", "./menu/presentation/Menu.tsx"),
 ] satisfies RouteConfig;

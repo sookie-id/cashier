3857c240d45aeaecf2d2dcb9f0686a2359927f95
Eusebius Marcel <emarcel@paystone.com>
2025-09-22 21:38:13 +0800

    Add some icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), svgr()],
   base: '/cashier/',
   ssr: {
     noExternal: ['styled-components'],
